966ecfba87a0cde9aba287e493ba19fa413166c8
Flavio Irani <54213802+coolfav@users.noreply.github.com>
2023-06-30 15:04:06 +0300

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -142,3 +142,15 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         data = response.get_json()
         self.assertEqual(len(data), 5)
+
+  def test_read_account(self):
+        """It should Get Account"""
+        test_account = AccountFactory()
+        resp = self.client.post(
+            BASE_URL,
+            json=test_account.serialize(),
+            content_type="application/json"
+        )
+        test_account = resp.get_json()
+        response = self.client.get(f"{BASE_URL}/{test_account['id']}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
